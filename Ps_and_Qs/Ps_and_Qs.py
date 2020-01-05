78a322323dbb95587618be493b7d7bf764028cc1
Gan <caoganyuan@gmail.com>
2020-01-05 15:26:32 -0700

    update Qs and Ps

---
+++ Ps_and_Qs/Ps_and_Qs.py
@@ -0,0 +1,25 @@
+# RSA - low entropy prime number 
+# paper reference: https://factorable.net/weakkeys12.extended.pdf
+
+from Crypto.PublicKey import RSA
+from Crypto.Util import number
+
+ciphertext = 0xf5ed9da29d8d260f22657e091f34eb930bc42f26f1e023f863ba13bee39071d1ea988ca62b9ad59d4f234fa7d682e22ce3194bbe5b801df3bd976db06b944da
+
+key1 = RSA.import_key(open('key1.pub').read())
+key2 = RSA.import_key(open('key2.pub').read())
+
+n1 = key1.n
+n2 = key2.n 
+
+e = key1.e
+
+p = number.GCD(n1,n2)
+q1 = n1 // p
+q2 = n2 // 1
+
+phi = (p - 1) * (q1 - 1)
+d = number.inverse(e, phi)
+
+plaintext = pow(ciphertext, d, n1)
+print(hex(plaintext))
